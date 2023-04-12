8af0c42dde22342e6b49d89a9726727548ffe0fd
yuragms <yura.gms@gmail.com>
2023-04-12 19:19:36 +0300

    nedfix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPageContext } from 'next';
-import { getSession, signOut } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 import useCurrentUser from '../hooks/useCurrentUser';
+import Navbar from '../components/Navbar';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,11 +23,12 @@ export default function Home() {
   const { data: user } = useCurrentUser();
   return (
     <>
-      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+      <Navbar />
+      {/* <h1 className="text-4xl text-green-500">Netflix Clone</h1>
       <p className="text-white">Logged in as : {user?.name}</p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>
         Logout!
-      </button>
+      </button> */}
     </>
   );
 }
