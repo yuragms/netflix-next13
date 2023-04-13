4bbba9fd2fddd943ddf4d7f602605a91d1f3a07e
yuragms <yura.gms@gmail.com>
2023-04-13 20:10:57 +0300

    trending

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
-import useCurrentUser from '../hooks/useCurrentUser';
 import Navbar from '../components/Navbar';
+import Billboard from '../components/Billboard';
+import MovieList from '../components/MovieList';
+import useMovieList from '../hooks/useMivieList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,10 +22,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
   return (
     <>
       <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
