af52addb24cadfe1067b204d54f39641914f60fc
yuragms <yura.gms@gmail.com>
2023-04-16 13:04:11 +0300

    vodal2

--- components/infoModal.tsx
+++ components/infoModal.tsx
@@ -53,12 +53,31 @@ const InfoModal: React.FC<InfoModalProps> = ({ visible, onClose }) => {
               loop
               poster={data?.thumbnalUrl}
               src={data?.videoUrl}
+            ></video>
+            <div
+              className="cursor-pointer absolute top-3 right-3 h-10 w-10 rounded-full bg-black bg-opacity-70 flex items-center justify-center"
+              onClick={() => {}}
             >
-
-            </video>
-              <div className="cursor-pointer absolute top-3 right-3 h-10 w-10 rounded-full bg-black bg-opacity-70 flex items-center justify-center" onClick={() => { }}>
-                <AiOutlineClose />
+              <AiOutlineClose className="text-white" size={20} />
+            </div>
+            <div
+              className="
+            absolute bottom-[10%] left-10"
+            >
+              <p className="text-white text-3xl md:text-4xl h-full lg:text-5xl font-bold mb-8">
+                {data?.title}
+              </p>
+              <div className="flex flex-row gap-4 items-center">
+                <PlayButton movieId={data?.id} />
+                <FavouriteButton movieId={data?.id} />
               </div>
+            </div>
+          </div>
+          <div className="px-12 py-8">
+            <p className="text-green-400 font-semibold text-lg">New</p>
+            <p className="text-white text-lg">{data?.duration}</p>
+            <p className="text-white text-lg">{data?.genre}</p>
+            <p className="text-white text-lg">{data?.description}</p>
           </div>
         </div>
       </div>
